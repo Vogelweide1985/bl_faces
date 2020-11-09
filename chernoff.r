348b146c3e086dec00361794b105283b40595eb2
vogelweide1985 <vogelweide1985@hotmail.de>
2020-11-09 20:47:05 +0100

    added chernoff features , half still missing

--- chernoff.r
+++ chernoff.r
@@ -39,10 +39,47 @@ df_bl_b <- map_df(df_bl_b, trim_table)
 df <- dplyr::left_join(df_mp, df_bl_a, by = c("Land"))   
 df <- dplyr::left_join(df, df_bl_b, by= c("Kürzel" = "Land"))  
 
+# Building variables
+
+# Building nose, #lenght of vorname und nachname
+temp <- data.frame(matrix(unlist(test ), nrow=length(test ), byrow=T))
+df[, "vorname"] <- as.character(temp[1,])
+df[, "nachname"] <- as.character(temp[2,])
+df[, "vorname_n"] <- nchar(df[, "vorname"])
+df[, "nachname_n"] <-nchar(df[, "nachname"])
+
+data.frame(matrix(unlist(test ), nrow=length(test ), byrow=T))
+
+
+#Bulding ear, 
+df$Geburts.datum <-gsub(".*♠","",df$Geburts.datum)
+df$Geburts.datum <- dmy(df$Geburts.datum)
+df$Geburts.datum <-  ymd(Sys.Date()) -  df$Geburts.datum 
+
+df$Amtsantritt <-  gsub(".*♠","",df$Amtsantritt)
+df$Amtsantritt <- dmy(df$Amtsantritt)
+df$Amtsantritt <-  ymd(Sys.Date()) -  df$Amtsantritt  
+
+
+#Building Mouth
+df$schulden_reduktion <- as.numeric(df$Schulden..31.12.2018.in.Mrd....20.)/ 
+   as.numeric(df$Schulden..2012.in.Mrd....18.) 
+
 
 #Building data frame for Chernoff faces
 chernoff <- matrix(nrow = 16, ncol = 15, rnorm(16 * 15))
 
+chernoff[, 1] <- as.numeric(gsub(",", ".", df[, "Ein.wohner.Mio...12."])) # Einwohner
+chernoff[, 2] <- df[, "Fläche.km²..12."] # Fläche
+chernoff[, 4] <- as.numeric(df$Schulden..31.12.2018.in.Mrd....20.) # Schulden 2018
+chernoff[, 5] <- as.numeric(df$Schulden..2012.in.Mrd....18.)  # Schulden 2018
+chernoff[, 6] <- df[, "schulden_reduktion"] # Prozentualer Schuldenrueckgang 2018/2012
+chernoff[, 12] <- df[, "vorname_n"] # Länge Vorname
+chernoff[, 13] <- df[, "nachname_n"] # Länge Nahcname
+chernoff[, 14] <- df[, "Geburts.datum"] # Amtsdauerin Tagen
+chernoff[, 15] <- df[, "Geburts.datum"] # Alter in Tagen
+
 #Chernoff plot
 
-faces(chernoff)
+faces(chernoff, scale = T, face.type = 0)
+
--- config.r
+++ config.r
@@ -3,6 +3,7 @@
 library(rvest)
 library(purrr)
 library(aplpack)
+library(lubridate)
 config <- list()
 
 config[["url"]] <- "https://de.wikipedia.org/wiki/Liste_der_Ministerpräsidenten_der_deutschen_Länder"
